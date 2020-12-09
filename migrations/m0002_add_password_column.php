a5b61de80b5acfadf446a84d996c8036989ace49
sksaju <sksaaju@gmail.com>
2020-12-10 03:30:27 +0600

    Implement Migrations

---
+++ migrations/m0002_add_password_column.php
@@ -0,0 +1,23 @@
+<?php
+
+use app\core\Application;
+
+/**  
+ * Class m0002_add_password_column 
+ * 
+ */
+
+class m0002_add_password_column
+{
+    public function up()
+    {
+        $db = Application::$app->db;
+        $db->pdo->exec("ALTER TABLE users ADD COLUMN pawwsrod VARCHAR(255) NOT NULL;");
+    }
+
+    public function down()
+    {
+        $db = Application::$app->db;
+        $db->pdo->exec("ALTER TABLE users DROP COLUMN pawwsrod;");
+    }
+}
